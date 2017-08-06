c9a016fe7a7a10ff36af989ba71a21d23d6bf605
bastien girschig <bastien.girschig@gmail.com>
2017-08-06 12:22:43 +0200

    Add button to "select picture"

--- src/main.js
+++ src/main.js
@@ -5,10 +5,28 @@ function getGreeting(){
 	return "hello";
 }
 
-if (typeof window !== 'undefined') {
+function init(){
+	// try server connection
 	fetch("https://icepeek.herokuapp.com/test", {mode: 'cors'})
 	.then(r=>r.text())
 	.then(text=>alert(text))
+
+	document.querySelector("#pitureSelectBtn").addEventListener("click", ()=>{
+		if(window.cordova) navigator.camera.getPicture(onSelectPicture, (err)=>console.log(err));
+		else console.log("looks like you're not running in cordova. cant select picture")
+	})
 }
+
+function onSelectPicture(url){
+	// set received image as background
+	console.log(url)
+	document.body.style.backgroundImage=`url(${url})`
+}
+
+if (typeof window !== 'undefined'){
+	if(window.cordova) document.addEventListener("deviceready", init, false);
+	else init()
+}
+
 
 module.exports.getGreeting = getGreeting;
