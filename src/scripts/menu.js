53962a89b28391b1e3f3e9bbb41bdf11374ce85e
bastien girschig <bastien.girschig@gmail.com>
2017-08-11 19:36:20 +0200

    re-indent lines in menu.js

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -6,23 +6,22 @@ document.getElementById("chat_tablink").addEventListener("click",function(){open
 
 function openTab(evt,tabcontainer){
 
-// Get all elements with class="tabcontent" and hide them
 
-tabcontent = document.getElementsByClassName("tabcontent");
-for (i = 0; i < tabcontent.length; i++) {
-tabcontent[i].style.display = "none";
-}
+	// Get all elements with class="tabcontent" and hide them
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+	}
 
-// Get all elements with class="tablinks" and remove the class "active"
-tablinks = document.getElementsByClassName("tablinks");
-for (i = 0; i < tablinks.length; i++) {
-tablinks[i].className = tablinks[i].className.replace(" active_tab", "");
-}
+	// Get all elements with class="tablinks" and remove the class "active"
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" active_tab", "");
+	}
 
-// Show the current tab, and add an "active" class to the button that opened the tab
-document.getElementById(tabcontainer).style.display = "block";
-evt.currentTarget.className += " active_tab";
-
+	// Show the current tab, and add an "active" class to the button that opened the tab
+	document.getElementById(tabcontainer).style.display = "block";
+	evt.currentTarget.className += " active_tab";
 }
 
 // Get the element with id="profile_tabcontent" and click on it to be the Default element displayed
